678e989b6d9ad26c388b228f05baeb4d1f9e24dd
Ferhad Mehdizade <frhdmehdiyev@gmail.com>
2022-08-02 23:48:26 +0400

    unnecessary directives removed

--- src/trie.c
+++ src/trie.c
@@ -4,12 +4,6 @@
 #include <assert.h>
 #include "trie.h"
 
-
-#define IS_CPTL_LTR(ch) (ch >= 65 && ch <= 90)
-#define IS_LWR_LTR(ch) (ch >= 97 && ch <= 122)
-#define IS_VALID_CHAR(ch) (IS_CPTL_LTR(ch) || IS_LWR_LTR(ch))
-#define NUMBER_OF_LETTERS 52
-
 static void traverse_trie(const node *n);
 static bool validate_word(const char *word);
 static node *put_node(node *parent, const char *word);
